20589a691effd497bde96afb34693895f394c177
PranayKumarGoru <goru.pranay@gmail.com>
2022-01-07 01:08:36 +0530

    Initial Commit

---
+++ src/main/java/com/relevel/porter/PorterAppCloneApplication.java
@@ -0,0 +1,13 @@
+package com.relevel.porter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PorterAppCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PorterAppCloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/relevel/porter/PorterAppCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.relevel.porter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PorterAppCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
